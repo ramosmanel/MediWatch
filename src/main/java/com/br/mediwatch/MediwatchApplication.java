9064ab79d0a27db94762352eb0ddbf64af8c2c3c
ramosmanel <emanuelramos20004@gmail.com>
2024-05-22 22:04:26 -0300

    feat: creating API environment

---
+++ src/main/java/com/br/mediwatch/MediwatchApplication.java
@@ -0,0 +1,13 @@
+package com.br.mediwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/mediwatch/MediwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mediwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
