a443e073218788b0896c91cab5fa05f3d78ba3eb
ramosmanel <emanuelramos20004@gmail.com>
2024-06-06 22:12:21 -0300

    feat: add findByName function for patient

--- src/main/java/com/br/mediwatch/repositories/PatientRepository.java
+++ src/main/java/com/br/mediwatch/repositories/PatientRepository.java
@@ -4,8 +4,10 @@ import com.br.mediwatch.models.PatientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PatientRepository extends JpaRepository<PatientModel, UUID> {
+    Optional<PatientModel> findByNamePatient(String patient);
 }
