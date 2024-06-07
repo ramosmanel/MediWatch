3742bb3f16ecedc4462b00161db7d19c48fe5415
ramosmanel <emanuelramos20004@gmail.com>
2024-06-06 22:13:28 -0300

    refactor: add exception handling for patients

--- src/main/java/com/br/mediwatch/services/ConsultationService.java
+++ src/main/java/com/br/mediwatch/services/ConsultationService.java
@@ -3,10 +3,13 @@ package com.br.mediwatch.services;
 import com.br.mediwatch.errors.ConsultationNotFoundException;
 import com.br.mediwatch.errors.DoctorNotFoundException;
 import com.br.mediwatch.errors.InvalidDoctorSpecialtyException;
+import com.br.mediwatch.errors.PatientNotFoundException;
 import com.br.mediwatch.models.ConsultationModel;
 import com.br.mediwatch.models.DoctorModel;
+import com.br.mediwatch.models.PatientModel;
 import com.br.mediwatch.repositories.ConsultationRepository;
 import com.br.mediwatch.repositories.DoctorRepository;
+import com.br.mediwatch.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +21,13 @@ import java.util.UUID;
 public class ConsultationService {
     private final ConsultationRepository consultationRepository;
     private final DoctorRepository doctorRepository;
+    private final PatientRepository patientRepository;
 
     @Autowired
-    public ConsultationService(ConsultationRepository consultationRepository, DoctorRepository doctorRepository) {
+    public ConsultationService(ConsultationRepository consultationRepository, DoctorRepository doctorRepository, PatientRepository patientRepository) {
         this.consultationRepository = consultationRepository;
         this.doctorRepository = doctorRepository;
+        this.patientRepository = patientRepository;
     }
 
     public List<ConsultationModel> getAllConsultation() {
@@ -41,6 +46,13 @@ public class ConsultationService {
             throw new InvalidDoctorSpecialtyException("The doctor is not an expert in the specialty provided.");
         }
 
+        Optional<PatientModel> optionalPatient = patientRepository.findByNamePatient(consultation.getNamePatient());
+
+        if(optionalPatient.isEmpty()) {
+            throw new PatientNotFoundException("Patient not found.");
+        }
+
+
         return consultationRepository.save(consultation);
     }
 
@@ -54,6 +66,12 @@ public class ConsultationService {
 
         if (!doctor.getSpecialtyDoctor().equals(updateConsultation.getSpecialtyConsultation())) {
             throw  new InvalidDoctorSpecialtyException("The doctor is not an expert in the specialty provided.");
+        }
+
+        Optional<PatientModel> optionalPatient = patientRepository.findByNamePatient(updateConsultation.getNamePatient());
+
+        if(optionalPatient.isEmpty()) {
+            throw new PatientNotFoundException("Patient not found.");
         }
 
         updateConsultation.setId(id);
